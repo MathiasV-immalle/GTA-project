d2cec346de0bf119e0f6e1114cdfb0e33c3153ee
Vansant Mathias <mathias.vansant@immalle.eu>
2017-03-23 13:21:59 +0100

    added GTA-Project

---
+++ GTA-project/GTA-project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTA_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voertuig v = new Voertuig(ConsoleColor.Magenta);
+            Voertuig truck = new Vrachtwagen();
+
+            v.Rij();
+            truck.Rij();
+            Console.ReadLine();
+        }
+    }
+}
